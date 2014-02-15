110f8f06d5deaef2a23e19850ef5cbb47b98a298
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-02-15 14:21:14 +0100

    started with longpoll transport

--- package/lib/longpoll.js
+++ package/lib/longpoll.js
@@ -20,24 +20,95 @@ var _RawLongPoll = function (url, protocols) {
    self._url = url;
 
    // our WebSocket shim with W3C API
-   var websocket = {};
+   var socket = {};
 
    // these will get defined by the specific shim
-   websocket.protocol = undefined;
-   websocket.send = undefined;
-   websocket.close = undefined;
+   socket.protocol = undefined;
+   socket.send = undefined;
+   socket.close = undefined;
 
    // these will get called by the shim.
    // in case user code doesn't override these, provide these NOPs
-   websocket.onmessage = function () {};
-   websocket.onopen = function () {};
-   websocket.onclose = function () {};
+   socket.onmessage = function () {};
+   socket.onopen = function () {};
+   socket.onclose = function () {};
 
-   websocket.send = function (msg) {
+   socket._send_buffer = [];
+   socket._sender = null;
+   socket._is_closing = false;
+
+
+   socket.close = function (code, reason) {
    };
 
-   websocket.close = function (code, reason) {
-   };
+
+   var options = {'protocols': ['wamp.2.msgpack', 'wamp.2.json']};
+
+   self.request('http://127.0.0.1:8080/wamp/open', options).then(
+      function (res) {
+         console.log("ok", res);
+         console.log(res.transport);
+
+         var txseq = 0;
+         var rxseq = 0;
+
+         socket.send = function (payload) {
+
+            socket._send_buffer.push(payload);
+            
+            if (!socket._sender) {
+               socket._sender = setInterval(function () {
+
+                  if (self._send_buffer.length) {
+
+                     var send_buffer = self._send_buffer.join('\x00');
+                     self._send_buffer = [];
+                     // send send_buffer .. 
+
+                     txseq += 1;
+                     self.request('http://127.0.0.1:8080/wamp/' + res.transport + '/send#' + txseq, send_buffer).then(
+                        function (res) {
+                           console.log("ok 2", res);
+                        },
+                        function (code, msg) {
+                           console.log("failed", code, msg);
+                        }
+                     );
+
+                  } else {
+                     clearInterval(socket._sender);
+                     socket._sender = null;
+                  }
+               }, 100);
+            }
+         };
+
+         function receive() {
+            rxseq += 1;
+            self.request('http://127.0.0.1:8080/wamp/' + res.transport + '/receive+' + rxseq).then(
+               function (res) {
+                  console.log("receive ok", res);
+                  if (!self._is_closing) {
+                     receive();
+                  }
+               },
+               function (code, msg) {
+                  console.log("receive failed", code, msg);
+                  if (!self._is_closing) {
+                     receive();
+                  }
+               }
+            );
+         }
+
+         receive();
+      },
+      function (code, msg) {
+         console.log("failed", code, msg);
+      }
+   );
+
+   return socket;
 };
 
 
