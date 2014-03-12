ec836f4da1f837eb94ba205186a3fc559db8207e
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-03-12 12:08:27 +0100

    add timeservice test example

---
+++ test/timeservice/frontend.js
@@ -0,0 +1,31 @@
+try {
+   var autobahn = require('autobahn');
+} catch (e) {
+   // when running in browser, AutobahnJS will
+   // be included without a module system
+}
+
+var connection = new autobahn.Connection({
+   url: 'ws://127.0.0.1:8080/ws',
+   realm: 'realm1'}
+);
+
+var session = null;
+
+connection.onopen = function (new_session) {
+
+   session = new_session;
+
+   session.call('com.timeservice.now').then(
+      function (now) {
+         console.log("Current time:", now);
+         //connection.close();
+      },
+      function (error) {
+         console.log("Call failed:", error);
+         //connection.close();
+      }
+   );
+};
+
+connection.open();
