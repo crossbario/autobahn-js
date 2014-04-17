794c3d9ef404d855dded98ce47c3ef2b37916c90
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-04-17 21:03:50 +0200

    improve test case

--- package/test/test_arguments.js
+++ package/test/test_arguments.js
@@ -1,14 +1,23 @@
 var autobahn = require('./../index.js'); 
 
+var _log = [];
 
-function ping() {      
+function track() {
+   console.log.apply(this, arguments);
+   _log.push(arguments);
+}
+
+function ping() {
+   track('ping()')
 }
 
 function add2(args) {
+   track('add2()', args);
    return args[0] + args[1];
 }
 
 function stars(args, kwargs) {
+   track('stars', args, kwargs);
    kwargs = kwargs || {};
    kwargs.nick = kwargs.nick || "somebody";
    kwargs.stars = kwargs.stars || 0;
@@ -19,12 +28,14 @@ var _orders = [];
 for (var i = 0; i < 50; ++i) _orders.push(i);
 
 function orders(args, kwargs) {
+   track('orders()', args, kwargs);
    kwargs = kwargs || {};
    kwargs.limit = kwargs.limit || 5;
    return _orders.slice(0, kwargs.limit);
 }
 
 function arglen(args, kwargs) {
+   track('arglen()', args, kwargs);
    args = args || [];
    kwargs = kwargs || {};
    return [args.length, Object.keys(kwargs).length];
@@ -45,76 +56,76 @@ function test1(session) {
 
    dl.push(session.call('com.arguments.ping').then(
       function () {
-         console.log("Pinged!");
+         track("Pinged!");
       }
    ));
 
    dl.push(session.call('com.arguments.add2', [2, 3]).then(
       function (res) {
-         console.log("Add2:", res);
+         track("Add2:", res);
       }
    ));
 
    dl.push(session.call('com.arguments.stars').then(
       function (res) {
-         console.log("Starred 1:", res);
+         track("Starred 1:", res);
       }
    ));
 
    dl.push(session.call('com.arguments.stars', [], {nick: 'Homer'}).then(
       function (res) {
-         console.log("Starred 2:", res);
+         track("Starred 2:", res);
       }
    ));
 
    dl.push(session.call('com.arguments.stars', [], {stars: 5}).then(
       function (res) {
-         console.log("Starred 3:", res);
+         track("Starred 3:", res);
       }
    ));
 
    dl.push(session.call('com.arguments.stars', [], {nick: 'Homer', stars: 5}).then(
       function (res) {
-         console.log("Starred 4:", res);
+         track("Starred 4:", res);
       }
    ));
 
    dl.push(session.call('com.arguments.orders', ['coffee']).then(
       function (res) {
-         console.log("Orders 1:", res);
+         track("Orders 1:", res);
       }
    ));
 
    dl.push(session.call('com.arguments.orders', ['coffee'], {limit: 10}).then(
       function (res) {
-         console.log("Orders 2:", res);
+         track("Orders 2:", res);
       },
       function (err) {
-         console.log(err);
+         track(err);
       }
    ));
 
    dl.push(session.call('com.arguments.arglen').then(
       function (res) {
-         console.log("Arglen 1", res);
+         track("Arglen 1", res);
       }
    ));
 
    dl.push(session.call('com.arguments.arglen', [1, 2, 3]).then(
       function (res) {
-         console.log("Arglen 2", res);
+         track("Arglen 2", res);
       }
    ));
 
    dl.push(session.call('com.arguments.arglen', [], {a: 1, b: 2, c: 3}).then(
       function (res) {
-         console.log("Arglen 3", res);
+         track("Arglen 3", res);
       }
    ));
 
    dl.push(session.call('com.arguments.arglen', [1, 2, 3], {a: 1, b: 2, c: 3}).then(
       function (res) {
-         console.log("Arglen 4", res);
+         track("Arglen 4", res);
       }
    ));
 
@@ -137,19 +148,23 @@ connection.onopen = function (session) {
 
    autobahn.when.all(pl1).then(
       function () {
-         console.log("All registered.");
+         track("All registered.");
 
          var pl2 = test1(session);
 
          autobahn.when.all(pl2).then(function () {
-            console.log("All finished.");
+            track("All finished.");
             connection.close();
          });
       },
       function () {
-         console.log("Registration failed!", arguments);
+         track("Registration failed!", arguments);
       }
    );  
+};
+
+connection.onclose = function () {
+   track(arguments);
 };
 
 connection.open();
