f747ccd7e88e26084e2b5cf940fa4072260aa383
arno <arnoschn@gmail.com>
2014-06-11 19:45:22 +0200

    fixes #72

---
+++ package/lib/polyfill.js
@@ -0,0 +1,8 @@
+require('./polyfill/object');
+require('./polyfill/array');
+require('./polyfill/string');
+require('./polyfill/function');
+require('./polyfill/console');
+require('./polyfill/typedarray');
+require('./polyfill/json');
+
---
+++ package/lib/polyfill/string.js
@@ -0,0 +1,7 @@
+// Add ECMA262-5 string trim if not supported natively
+//
+if (!('trim' in String.prototype)) {
+    String.prototype.trim= function() {
+        return this.replace(/^\s+/, '').replace(/\s+$/, '');
+    };
+}
