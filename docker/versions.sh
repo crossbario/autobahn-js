6470466a579202b0df0191f7800eb80767c7b868
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-10-21 11:34:30 +0200

    update version

--- docker/versions.sh
+++ docker/versions.sh
@@ -3,7 +3,7 @@
 #
 # CHANGE FOR NEW RELEASES (these need to be proper Git tags in the respective repo):
 #
-export AUTOBAHN_JS_VERSION='18.3.2'
+export AUTOBAHN_JS_VERSION='18.10.2'
 #
 # END OF CONFIG
 #
@@ -12,6 +12,7 @@ export AUTOBAHN_JS_VERSION='18.3.2'
 # Git working directories of all relevant repos must reside
 # in parallel (as siblings) to this repository
 #
+export AUTOBAHN_PYTHON_VERSION=$(grep -E '^(__version__)' ../autobahn/_version.py | cut -d ' ' -f3 | sed -e 's|[u"'\'']||g')
 export AUTOBAHN_JS_VCS_REF=`git --git-dir="../.git" rev-list -n 1 v${AUTOBAHN_JS_VERSION} --abbrev-commit`
 export BUILD_DATE=`date -u +"%Y-%m-%d"`
 
