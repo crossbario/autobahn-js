258156425f89fc03331b42f202c79c6cbcbd85eb
Omer Akram <om26er@gmail.com>
2019-05-08 03:35:41 +0500

    More seller code

--- sample.js
+++ sample.js
@@ -4,3 +4,4 @@ var seller = require('./lib/xbr/seller.js');
 
 simple_seller = new seller.SimpleSeller("0x3a1076bf45ab87712ad64ccb3b10217737f7faacbf2872e88fdd9a537d8fe266", 100);
 simple_seller.start("Hello");
+console.log(simple_seller._session);
