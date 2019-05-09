089e214f39e37f80cf8aff2a7852a1dc8ca0c7a6
Omer Akram <om26er@gmail.com>
2019-05-10 04:24:49 +0500

    Do some stuff on the network

--- sample.js
+++ sample.js
@@ -2,6 +2,21 @@ var autobahn = require('./lib/autobahn.js');
 var seller = require('./lib/xbr/seller.js');
 
 
-simple_seller = new seller.SimpleSeller("0x3a1076bf45ab87712ad64ccb3b10217737f7faacbf2872e88fdd9a537d8fe266", 100);
-simple_seller.start("Hello");
-console.log(simple_seller._session);
+var connection = new autobahn.Connection({
+   url: "ws://localhost:8080/ws",
+   realm: "realm1"
+});
+
+connection.onopen = function (session, details) {
+    console.log("OPENED");
+    var simple_seller = new seller.SimpleSeller("0x3a1076bf45ab87712ad64ccb3b10217737f7faacbf2872e88fdd9a537d8fe266", 100);
+    simple_seller.start(session);
+    console.log(simple_seller._session);
+};
+
+connection.onclose = function (reason, details) {
+    console.log(reason);
+    console.log(details);
+};
+
+connection.open();
