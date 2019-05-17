2313b52b399ed96c486fdc16c253f5481ef4dd39
Omer Akram <om26er@gmail.com>
2019-05-17 18:19:58 +0500

    bring things closer to Python impl

--- sample.js
+++ sample.js
@@ -1,5 +1,6 @@
 var autobahn = require('./lib/autobahn.js');
 var seller = require('./lib/xbr/seller.js');
+var series = require('./lib/xbr/keyseries.js');
 
 
 var connection = new autobahn.Connection({
@@ -7,10 +8,18 @@ var connection = new autobahn.Connection({
    realm: "realm1"
 });
 
+var onRotate = function(series) {
+    console.log("ROTATED");
+};
+
 connection.onopen = function (session, details) {
     console.log("OPENED");
-    var simple_seller = new seller.SimpleSeller("0x3a1076bf45ab87712ad64ccb3b10217737f7faacbf2872e88fdd9a537d8fe266", 100);
-    simple_seller.start(session);
+    // var simple_seller = new seller.SimpleSeller("0x3a1076bf45ab87712ad64ccb3b10217737f7faacbf2872e88fdd9a537d8fe266", 100);
+    // simple_seller.start(session);
+    var s = new series.KeySeries("OMER", 10, 10000, "io.crossbar", onRotate);
+    s.start();
+    var encrypted = s.encrypt("adsas");
+    console.log(encrypted);
 };
 
 connection.onclose = function (reason, details) {
