8bab3ece371ebc14b3a1f01e47f903d9dc64121c
Omer Akram <om26er@gmail.com>
2019-05-19 19:31:53 +0500

    Bring the XBR Seller near completion

--- sample.js
+++ sample.js
@@ -1,11 +1,11 @@
 var autobahn = require('./lib/autobahn.js');
 var seller = require('./lib/xbr/seller.js');
-var series = require('./lib/xbr/keyseries.js');
 
 
 var connection = new autobahn.Connection({
    url: "ws://localhost:8080/ws",
-   realm: "realm1"
+   realm: "realm1",
+    serializers: [new autobahn.serializer.CBORSerializer()]
 });
 
 var onRotate = function(series) {
@@ -15,12 +15,8 @@ var onRotate = function(series) {
 connection.onopen = function (session, details) {
     console.log("OPENED");
     var simple_seller = new seller.SimpleSeller("0x3a1076bf45ab87712ad64ccb3b10217737f7faacbf2872e88fdd9a537d8fe266", 100);
-    simple_seller.add(new Buffer("THIS"), "io.crossbar", 10, 10000);
+    simple_seller.add(autobahn.nacl.randomBytes(16), "io.crossbar", 10, 10000);
     simple_seller.start(session);
-    // var s = new series.KeySeries("OMER", 10, 10000, "io.crossbar", onRotate);
-    // s.start();
-    // var encrypted = s.encrypt("adsas");
-    // console.log(encrypted);
 };
 
 connection.onclose = function (reason, details) {
