ef6c75969633b5c52f1be6638a1536b62fee539b
James Hilliard <james.hilliard1@gmail.com>
2019-07-13 18:45:55 -0600

    Remove useless xbr dependencies for normal autobahn builds and improve scons env autodetection.

---
+++ index-xbr.js
@@ -0,0 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  AutobahnJS - http://autobahn.ws, http://wamp.ws
+//
+//  A JavaScript library for WAMP ("The Web Application Messaging Protocol").
+//
+//  Copyright (c) Crossbar.io Technologies GmbH and contributors
+//
+//  Licensed under the MIT License.
+//  http://www.opensource.org/licenses/mit-license.php
+//
+///////////////////////////////////////////////////////////////////////////////
+
+module.exports = require('./lib/xbr.js');
