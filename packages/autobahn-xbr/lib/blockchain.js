27373b6b25e4a82eec6161159092314eb3eabe8a
Omer Akram <om26er@gmail.com>
2019-08-30 03:08:23 +0500

    Make eth stuff into a separate file

--- packages/autobahn-xbr/lib/blockchain.js
+++ packages/autobahn-xbr/lib/blockchain.js
@@ -11,3 +11,30 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
+var Web3 = require("web3");
+var xbr = require('./autobahn-xbr');
+
+var SimpleBlockchain = function (gateway) {
+    this.gateway = gateway;
+    this.w3 = null;
+};
+
+SimpleBlockchain.prototype.start = function() {
+    if (this.gateway != null) {
+        return;
+    }
+
+    if (this.gateway == null) {
+        this.w3 = new Web3(Web3.currentProvider);
+    } else {
+        this.w3 = new Web3(new Web3.providers.HttpProvider(this.gateway));
+    }
+
+    if (this.w3.isConnected()) {
+        throw `could not connect to Web3/Ethereum at: ${this.gateway || 'auto'}`;
+    } else {
+        console.log(`Connected to network ${this.w3.version.network} at provider ${this.gateway || 'auto'}`)
+    }
+};
+
+exports.SimpleBlockchain = SimpleBlockchain;
---
+++ packages/autobahn-xbr/lib/ethereum.js
@@ -0,0 +1,91 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  XBR Open Data Markets - https://xbr.network
+//
+//  JavaScript client library for the XBR Network.
+//
+//  Copyright (C) Crossbar.io Technologies GmbH and contributors
+//
+//  Licensed under the Apache 2.0 License:
+//  https://opensource.org/licenses/Apache-2.0
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// https://truffleframework.com/docs/truffle/getting-started/package-management-via-npm#within-javascript-code
+var contract = require("truffle-contract");
+const uuidv4 = require('uuid/v4');
+
+var XBRToken_json = require("./contracts/XBRToken.json");
+var XBRNetwork_json = require("./contracts/XBRNetwork.json");
+var XBRPaymentChannel_json = require("./contracts/XBRPaymentChannel.json");
+
+var XBRToken = contract(XBRToken_json);
+var XBRNetwork = contract(XBRNetwork_json);
+var XBRPaymentChannel = contract(XBRPaymentChannel_json);
+
+// a Web3 provider must be set by the user first to use this library
+var setProvider = async function (provider) {
+    XBRToken.setProvider(provider);
+    XBRNetwork.setProvider(provider);
+    XBRPaymentChannel.setProvider(provider);
+    await init_xbr();
+};
+
+var uuid = function () {
+    const buffer = [];
+    uuidv4(null, buffer);
+    return buffer;
+};
+
+var MemberLevel = {
+    NONE: 0,
+    ACTIVE: 1,
+    VERIFIED: 2,
+    RETIRED: 3,
+    PENALTY: 4,
+    BLOCKED: 5,
+};
+var NodeType = {
+    NONE: 0,
+    MASTER: 1,
+    CORE: 2,
+    EDGE: 3,
+};
+var ActorType = {
+    NONE: 0,
+    NETWORK: 1,
+    MARKET: 2,
+    PROVIDER: 3,
+    CONSUMER: 4,
+};
+//
+// as long as we haven't deployed the XBR smart contracts to
+// any public network (testnets or mainnet), a user must set the
+// addresses of our deployed token and network smart contracts
+// on the (private) network the user is connecting to and where
+// the XBR contracts need to be deployed
+//
+async function init_xbr() {
+    if ('XBR_DEBUG_TOKEN_ADDR' in global && XBR_DEBUG_TOKEN_ADDR) {
+        exports.xbrtoken = await XBRToken.at(XBR_DEBUG_TOKEN_ADDR);
+    } else {
+        console.log('WARNING: The XBR smart contracts are not yet depoyed to public networks. Please set XBR_DEBUG_TOKEN_ADDR manually.');
+        exports.xbrtoken = await XBRToken.at("0xcfeb869f69431e42cdb54a4f4f105c19c080a601");
+    }
+
+    if ('XBR_DEBUG_NETWORK_ADDR' in global && XBR_DEBUG_NETWORK_ADDR) {
+        exports.xbrnetwork = await XBRNetwork.at(XBR_DEBUG_NETWORK_ADDR);
+    } else {
+        console.log('WARNING: The XBR smart contracts are not yet depoyed to public networks. Please set XBR_DEBUG_NETWORK_ADDR manually.');
+        exports.xbrnetwork = await XBRNetwork.at("0x254dffcd3277c0b1660f6d42efbb754edababc2b");
+    }
+}
+
+exports.XBRToken = XBRToken;
+exports.XBRNetwork = XBRNetwork;
+exports.XBRPaymentChannel = XBRPaymentChannel;
+exports.MemberLevel = MemberLevel;
+exports.NodeType = NodeType;
+exports.ActorType = ActorType;
+exports.setProvider = setProvider;
+exports.uuid = uuid;
