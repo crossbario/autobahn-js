cc3950c7b548a69d0214af44dddea22ff80d4da1
Tobias Oberstein <tobias.oberstein@crossbario.com>
2019-09-15 11:18:32 +0200

    include ABJS-XBR in docker image builds

--- docker/versions.sh
+++ docker/versions.sh
@@ -3,7 +3,8 @@
 #
 # CHANGE FOR NEW RELEASES (these need to be proper Git tags in the respective repo):
 #
-export AUTOBAHN_JS_VERSION='18.10.2'
+export AUTOBAHN_JS_VERSION='19.9.2'
+export AUTOBAHN_JS_XBR_VERSION='19.9.3'
 #
 # END OF CONFIG
 #
@@ -12,7 +13,7 @@ export AUTOBAHN_JS_VERSION='18.10.2'
 # Git working directories of all relevant repos must reside
 # in parallel (as siblings) to this repository
 #
-export AUTOBAHN_PYTHON_VERSION=$(grep -E '^(__version__)' ../autobahn/_version.py | cut -d ' ' -f3 | sed -e 's|[u"'\'']||g')
+# export AUTOBAHN_PYTHON_VERSION=$(grep -E '^(__version__)' ../autobahn/_version.py | cut -d ' ' -f3 | sed -e 's|[u"'\'']||g')
 export AUTOBAHN_JS_VCS_REF=`git --git-dir="../.git" rev-list -n 1 v${AUTOBAHN_JS_VERSION} --abbrev-commit`
 export BUILD_DATE=`date -u +"%Y-%m-%d"`
 
@@ -20,4 +21,5 @@ echo ""
 echo "The Crossbar.io Project (build date ${BUILD_DATE})"
 echo ""
 echo "autobahn-js ${AUTOBAHN_JS_VERSION} [${AUTOBAHN_JS_VCS_REF}]"
+echo "autobahn-js-xbr ${AUTOBAHN_JS_XBR_VERSION} [${AUTOBAHN_JS_VCS_REF}]"
 echo ""
